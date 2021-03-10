620b44a87b9b6e2e86adc76bfd90ec60919b0a26
Joshua Noakes <joshua.noakes1@icloud.com>
2021-03-10 00:02:29 +0000

    wol should work

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 //loading express and morgan
+const http = require('http');
+require('dotenv').config()
+const port = process.env.port || 3001;
 const express = require('express');
 const app = express();
+const server = http.createServer(app);
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const {
-    spawn
-} = require('child_process');
+const wol = require('wol');
+
+server.listen(port);
+console.log(`🚀 Started server on port ${port} 🚀`);
 
 // Console log of anything hitting our server 
 app.use(morgan('dev'));
@@ -21,14 +26,21 @@ app.use((req, res, next) => {
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.header('Access-Control-Allow-Methods', 'POST');
     }
     next();
 });
 
 app.post('/wol', (req, res) => {
-
     // TODO: Add post password protection
+    if (req.body.pass != process.env.password) {
+        res.status(404).json({
+            error: {
+                message: "Incorrect Mac Address Format | Mac Address must be in format \"01:23:45:67:89:AB\""
+            }
+        });
+        return;
+    }
 
     // if no mac address was sent
     if (!req.body.mac) {
@@ -40,7 +52,7 @@ app.post('/wol', (req, res) => {
         return;
     }
 
-    // chceking to see if its a real mac address
+    // chceking to see if its a good mac address
     if (!req.body.mac.toString().match(/^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$/g)) {
         res.status(404).json({
             error: {
@@ -50,13 +62,20 @@ app.post('/wol', (req, res) => {
         return;
     }
 
-    const wol = spawn("echo", ["lol"]);
+    wol.wake(`${req.body.mac}`, function (err, res) {
+        if (err) {
+            res.status(500).json({
+                error: "WakeOnLan Error",
+                message: error
+            });
+            return;
+        }
+    });
 
     res.status(200).json({
-        message: "Lol"
+        message: `Sent WOL request to ${req.body.mac}`
     });
 });
-
 
 // Errrors 
 // Handle 404 not found
@@ -65,7 +84,7 @@ app.use((req, res, next) => {
     error.status = 404;
     next(error);
 });
-// Handle anything else thats not a 404 now found
+// Handle anything else thats not a 404 not found
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({
         error: {
@@ -73,4 +92,3 @@ app.use((error, req, res, next) => {
         }
     });
 });
-module.exports = app;
