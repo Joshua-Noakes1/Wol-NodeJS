ba8369feaf787b406d6887b72960efd94a72ff61
Joshua Noakes <joshua.noakes1@icloud.com>
2022-02-05 18:18:25 +0000

    add api router

--- app.js
+++ app.js
@@ -1,52 +1,35 @@
-// App config
-const express = require("express");
+const lcl = require('cli-color'),
+    compression = require('compression'),
+    cors = require('cors'),
+    morgan = require('morgan'),
+    bodyParser = require("body-parser"),
+    favicon = require('serve-favicon')
+express = require('express');
+
+// const global app object
 const app = express();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const path = require('path');
-const favicon = require('serve-favicon');
 
-// Express dev
-app.use(morgan("dev"));
-
-// favicon
-app.use(favicon(path.join(__dirname, 'public', 'static', 'favicon.ico')))
-
-// register ejs
-app.set('view engine', 'ejs');
-app.set('views', 'public');
-
-// Express bodyparser
-app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    })
-);
+// express middlewares
+app.use(compression());
+app.use(cors());
+app.use(morgan('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(favicon(path.join(__dirname, 'public', 'static', 'favicon.ico')));
 
-// Modules
-const wol = require('./lib/wol/wakeOnLan');
-const check = require('./lib/wol/checkIP');
+// routes
+app.use('/api', require('./api/router'));
 
-// Endpoints
-app.use('/wol', wol);
-app.use('/check', check);
-
-// Status endpoint
-app.get("/status", (req, res) => {
-    res.status(200).json({
-        "success": "true",
-        "message": "🚀 The rocket has launched 🚀",
-    });
-});
-
-// Express error handling 
+// 404 - Page Not Found
 app.use((req, res, next) => {
     const error = new Error("❌ Page Not Found ❌")
     error.status = 404;
     next(error);
 });
 
+// 500 - Internal Server Error
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({
         "success": "false",
@@ -57,4 +40,8 @@ app.use((error, req, res, next) => {
     });
 });
 
-module.exports = app;
+// Start the server
+const port = process.env.port || 3000;
+app.listen(port, async function () {
+    console.log(lcl.blue("[Info]"), "Server started on port", lcl.yellow(port));
+});
